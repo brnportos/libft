b9265478ae7b8e0e22b012e0fffffc95be637262
Portos <brnportos>
2026-01-04 12:40:24 +0300

    fix(ft_bzero):use ft_memset to set memory and verify usage

--- libft/ft_bzero/ft_bzero/ft_bzero.c
+++ libft/ft_bzero/ft_bzero/ft_bzero.c
@@ -3,10 +3,7 @@
 void	ft_bzero(void *buffer, size_t len)
 {
 	unsigned char *ptr;
-	size_t		i;
 
 	ptr = (unsigned char *)buffer;
-	i = 0;
-	while(i < len)
-		ptr[i++] = 0;
+	ft_memset(ptr, 0, len);
 }
