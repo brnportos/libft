4adaf84f43c460a329045e032ab1989e48b81ac4
brnportos <brnoportos@gmail.com>
2026-02-10 20:39:25 +0300

    feat(libft): add ft_memchr implementation

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -22,5 +22,6 @@ int     ft_tolower(int c);
 char    *ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
+void    *ft_memchr(const void *s, int c, size_t n);
 
 #endif
---
+++ libft/src/ft_memchr.c
@@ -0,0 +1,19 @@
+#include "../include/libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*s_tmp;
+	unsigned char	c_tmp;
+
+	if (!s)
+		return ;
+	s_tmp = (unsigned char *)s;
+	c_tmp = (unsigned char)c;
+	while(*s_tmp &&  n-- > 0)
+	{
+		if (*s_tmp == c_tmp)
+			return ((void *)s_tmp);
+		s_tmp++;
+	}
+	return (NULL);
+}
