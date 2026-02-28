b3fbed9f7cca9f3c6eee589ec70557bc507e2002
brnportos <brnoportos@gmail.com>
2026-02-28 21:21:15 +0300

    feat(libft): add ft_split implementation

--- Milest0/libft/include/libft.h
+++ Milest0/libft/include/libft.h
@@ -1,10 +1,6 @@
 #ifndef LIBFT_H
 #define LIBFT_H
 
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define RESET   "\033[0m"
-
 #include <unistd.h>
 #include <stdlib.h>
 
@@ -34,5 +30,6 @@ void    *ft_calloc(size_t nmemb, size_t size);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
 char    *ft_strjoin(char const *s1, char const *s2);
 char    *ft_strtrim(char const *s1, char const *set);
+char    **ft_split(char const *s, char c);
 
 #endif
