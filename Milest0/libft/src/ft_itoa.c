ecb62c6ceedb8fbced5559ffdff9a28cac9e9e37
brnportos <brnoportos@gmail.com>
2026-03-02 21:16:45 +0300

    feat(libft): add ft_itoa implementation

---
+++ Milest0/libft/src/ft_itoa.c
@@ -0,0 +1,54 @@
+#include "../include/libft.h"
+
+static int  ft_len(int n)
+{
+    int len;
+
+    len = 0;
+    if (n < 0)
+        len = 1;
+    while (n)
+    {
+        n /= 10;
+        len++;
+    }
+    return (len);
+}
+
+static long ft_sign_checking(long n_tmp)
+{
+    long    nbr;
+
+    if (n_tmp < 0)
+        nbr = -n_tmp;
+    else
+        nbr = n_tmp;
+    return (nbr);
+}
+
+char    *ft_itoa(int n)
+{
+    char    *str;
+    long    n_tmp;
+    long    nbr;
+    int     len;
+
+    n_tmp = n;
+    len = ft_len(n_tmp);
+    str = malloc(sizeof * str * len + 1);
+    if (!str)
+        return (NULL);
+    str[len] = '\0';
+    nbr = ft_sign_checking(n_tmp);
+    while (len > 0)
+    {
+        str[len -1] = (nbr % 10) + '0';
+        nbr /= 10;
+        if (n_tmp < 0 && len == 1)
+            break;
+        len--;
+    }
+    if (n_tmp < 0)
+        str[0] = '-';
+    return (str);
+}
