eb7ef4138d50e105666ec775ca8d13fea919e388
brnportos <brnoportos@gmail.com>
2026-03-17 20:27:55 +0300

    fix(libft): fix ft_lastadd_back empty list or null guard

--- Mileston 00/libft/src/ft_lstadd_back.c
+++ Mileston 00/libft/src/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: portos <portos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 19:51:10 by portos            #+#    #+#             */
-/*   Updated: 2026/03/17 19:59:15 by portos           ###   ########.fr       */
+/*   Updated: 2026/03/17 20:24:45 by portos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ void    ft_lstadd_back(t_list **lst, t_list *new)
 {
     t_list  *last;
     
-    if (!*lst || !lst && new)
-        *lst = new;
-    else if (!new)
+    if (!lst && !new)
         return ;
+    else if (!*lst)
+        *lst = new;
     else
     {
         last = ft_lstlast(*lst);
