1a3d79bd92ae5e2685e54a4720940cdcbcc46c2d
brnportos <brnoportos@gmail.com>
2026-03-19 20:54:05 +0300

    feat(libft): add ft_lstclear implementation

--- Mileston 00/libft/include/libft.h
+++ Mileston 00/libft/include/libft.h
@@ -6,7 +6,7 @@
 /*   By: portos <portos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 18:52:27 by portos            #+#    #+#             */
-/*   Updated: 2026/03/18 20:35:51 by portos           ###   ########.fr       */
+/*   Updated: 2026/03/19 20:39:53 by portos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,7 @@ void    free_single_lst(t_list **root);
 t_list  *ft_lstlast(t_list *lst);
 void    ft_lstadd_back(t_list **lst, t_list *new);
 void    ft_lstdelone(t_list *lst, void (*del)(void *));
+void    ft_lstclear(t_list **lst, void (*del)(void *));
 void    free_doubly_lst(t_node **tail, t_node **head);
 
 #endif
