2ae5ee245736cd904f114e622d58ec3a7be6c498
brnportos <brnoportos@gmail.com>
2026-04-04 20:26:23 +0300

    feat(exam): add alpha_mirror implementation

---
+++ Exam/rank2/level2/alpha_mirror/alpha_mirror.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		int	i;
+
+		i = 0;
+		while (av[1][i])
+		{
+			if (av[1][i] >= 'a' && av[1][i] <= 'z')
+			{
+				char	alpha = 'z' - av[1][i] + 'a';
+				ft_putchar(alpha);
+			}
+			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+			{
+				char	alp = 'Z' - av[1][i] + 'A';
+				ft_putchar(alp);
+			}
+			else
+				ft_putchar(av[1][i]);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+}
