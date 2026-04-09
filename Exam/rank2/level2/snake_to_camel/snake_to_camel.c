4c122ad07f01a64de2f9df02608869624e551a45
brnportos <brnoportos@gmail.com>
2026-04-09 17:02:29 +0300

    feat(exam): add snake_to_camel implementation

---
+++ Exam/rank2/level2/snake_to_camel/snake_to_camel.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		int	i = 0;
+
+		while(av[1][i])
+		{
+			if (av[1][i] == '_')
+			{
+				i++;
+				ft_putchar(av[1][i] - 32);
+			}
+			else
+				write(1, &av[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+}
