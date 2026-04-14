535f618a071b46b7777e1d5e387b7eca29387640
brnportos <brnoportos@gmail.com>
2026-04-14 21:10:23 +0300

    feat(GNL): add get_next_line ft_strlen implementation

--- Mileston 01/get_next_line/get_next_line_utils.c
+++ Mileston 01/get_next_line/get_next_line_utils.c
@@ -1,11 +1,23 @@
 #include "get_next_line"
 
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
 char *ft_strdup(char *dest, char *src)
 {
     int i;
     char    *dup;
     int len;
 
+    if (!dest || !src)
+        return (NULL);
     len = ft_strlen(str);
     i = 0;
     dup = malloc(len + 1)
