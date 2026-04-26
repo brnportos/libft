f86ca0e9ef236aa037287a3f51f8f69a5ce7721f
brnportos <brnoportos@gmail.com>
2026-04-26 19:18:21 +0300

    feat(ft_printf): add ft_putnbr of ft_printf implementation

---
+++ Mileston 01/ft_printf/ft_putnbr.c
@@ -0,0 +1,21 @@
+#include "ft_printf.h"
+
+void    ft_putnbr(int n, int *len)
+{
+    if (n == -2147483648)
+        ft_putchar(n, len);
+    else if(n < 0)
+    {
+        write(1, "-", 1);
+        n *= -1;
+        (*len)++;
+        ft_putnbr(n, len);
+    }
+    else if(n >= 10)
+    {
+        ft_putnbr(n / 10, len);
+        ft_putnbr(n % 10, len);
+    }
+    else
+        ft_putchar(n + '0', len);
+}
